05b751def468280c8ae06dd87719061c41f34480
caiotheodoro <caio-eduardotheodoro@hotmail.com>
2022-04-19 02:20:04 -0300

    refactor: 💡 udp2 refact

--- Programação com sockets UDP/UDP1/cliente.py
+++ Programação com sockets UDP/UDP1/cliente.py
@@ -1,147 +1,89 @@
-'''
-    Questão 1 - UDP CLIENT 
-    Autor:Gustavo Kioshi Asato | Caio Theodoro da Silva
-    Data de criação: 18/04/2022
-    Descrição: O programa é um chat P2P, no qual 2 clientes se conectam via socket e enviam mensagem entre si.
-    As mensagem enviadas serão enviadas com um cabeçalho seguindo o seguinte formato:
-    - tipo de mensagem [1 byte]
-    - tamanho apelido (tam_apl) [1 byte]
-    - apelido [tam_apl (1 a 64) bytes ] 
-    - tamanho mensagem (tam_msg) [1 byte]
-    - mensagem [tam_msg bytes]
-    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-    |   Tipo de mensagem   |  Tamanho apelido  |  apelido   |
-    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-    |        tamanho mensagem      |        mensagem        |
-    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-'''
-
-
-
-
-
-
-import re
-import socket
-import sys
 import threading
+from threading import Thread
+import sys
+import socket
+
+
 
 
 ip = "127.0.0.1"
-porta = [1234, 4323,3233]
-
-#Cria um socket do tipo UCP os parametros AF_INET e SOCK_DGRAM definem
-#qual a familia de endereços será utilizada e qual o tipo de socket
-socketClient = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#Função para verificar se uma mensagem é uma url
-def verificaURL(url):
-    #Regex para verificar se a mensagem é uma url 
-    regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
-    return re.match(regex, url)#retorna a url
+address = [5761, 5762, 5763]
 
 
-#Função para receber mensagens
-def mensagemRecebe(ip, port):
-    # Socket cliente
-    print((ip, port))
-    socketClient.bind((ip, int(port)))
+def receptor(player, address):
+    s.bind((ip, address))
     while True:
-        try:
-            # Recebe a mensagem para a variáveis mensage, ipAddress 
-            # 332 é o tamanho total que a mensagem possui
-            mensagem, ipAddress = socketClient.recvfrom(322)
-            print('')
-            # Atribui o tipo de mensagem para
-            mensagemTipo = int(mensagem[0])
-            # Atribui o tamanho do nome para a variável nomeTamanho
-            nomeTamanho = int(mensagem[1])
-            # Atribui o nick do cliente para a variável nick
-            nome = mensagem[2:nomeTamanho+2].decode()
-            # Atribui o tamanho da mensagem para a variável mensageTamanho
-            mensagemTamanho = mensagem[nomeTamanho+2]
-            mensagePosition = int(nomeTamanho + 3)
-            # Atribui a mensagem para a variável mensageContent
-            mensageContent = mensagem[mensagePosition : (mensagePosition + mensagemTamanho)].decode()
-            # Verifica se a mensagem é do tipo ECHO
-            if mensagemTipo == 4:
-                # Formata a mensagem para reenviar
-                mensagem = (mensagemTipo.to_bytes(1,'big') + nomeTamanho.to_bytes(1,'big') + nome.encode() + mensagemTamanho.to_bytes(1,'big') + mensageContent.encode())
-                # Envia a mensagem
-                socketClient.sendto(mensagem, ipAddress)
-                # Não ficar no loop do ECHO
-                mensagemTipo =1
-            # Verifica se a mensagem é uma url
-            elif mensagemTipo == 3:
-                print(nome + ':Link:' + mensageContent)
-            # Verifica se a mensagem possui emojis
-            elif mensagemTipo == 2:
-                 print(nome + ':Emoji:' + mensageContent)
-            # Verifica se a mensagem é do tipo normal
-            else:
-                print(nome + ':Mensagem:' + mensageContent)
-        except:
-            print("")
+        msg, addr = s.recvfrom(1024)
+        tipo = int(msg[0])
+        tamPlayer = int(msg[1])
+        player = msg[2:2+tamPlayer].decode("utf-8")
+        tamMsg = int(msg[tamPlayer+2])
+        msgPos = tamPlayer+3
+        msg = msg[msgPos:msgPos+tamMsg].decode("utf-8")
 
-# Função para enviar mensagens
-def mensagenEnvia(ip, port):
-    addr = ip, port
-    while True:
-        #Recebe a mensagem do cliente
-        mensagem = input("")
-        # Atribui o tipo de mensagem para 1
-        mensagemTipo = 1
-        # Atribui o tamanho da mensagem para a variável mensagemTamanho
-        mensagemTamanho= len(mensagem.encode())
-        # Atribui o tamanho do apelido para a variável nickSize
-        nicknameSize= len(apelido.encode())
-        # Verifica se a mensagem é menor do que 255 bytes
-        if len(mensagem.encode()) > 255:
-            print("Mensage too long")
+        if tipo == 4:
+            msg = (1).to_bytes(1, byteorder="big") + tamPlayer.to_bytes(1, byteorder="big") + player.encode("utf-8") + tamMsg.to_bytes(1, byteorder="big") + msg.encode("utf-8")
+            s.sendto(msg, (ip, address))
         else:
-            # Verifica se a mensagem possui emojis
-            if(mensagem.find("\o/" ) == 0):
-                 mensagemTipo = 2
-            # Verifica se a mensagem é uma url
-            elif(verificaURL(mensagem) != None):
-                mensagemTipo = 3
-            # Verifica se a mensagem é do tipo ECHO
-            elif(mensagem.find("ECHO") == 0):
-                mensagemTipo = 4
-            # Formata a mensagem para enviar
-            mensagem = (mensagemTipo.to_bytes(1,'big') + nicknameSize.to_bytes(1,'big') + apelido.encode() + mensagemTamanho.to_bytes(1,'big') + mensagem.encode())
-            # Envia a mensagem
-            socketClient.sendto(mensagem, addr)
+            print(f"{player}: {msg}")
+        
 
 
-def main():
-    # recebe qual tipo de cliente que esta acessando
-    idx = sys.argv[1]
 
-    #Recebe a mensagem do cliente
-    global apelido
-    # Recebe o apelido do cliente
-    apelido = input("Digite seu nome: ")
+def enviador(player, address):
+    tipo = 1
+    while True:
+        msg = input("~ ")
+        tamMsg = len(msg.encode("utf-8"))
+        tamPlayer = len(player.encode("utf-8"))
+        if len(msg.encode("utf-8")) > 254:
+            print("Mensagem muito grande")
+            continue
+        else:  
+            if msg.encode("utf-8").find(b'\xF0\x9F\x98\x81') != -1:
+                tipo = 2
+            if msg.encode("utf-8").find(b'http') != -1:
+                tipo = 3
+            if msg.encode("utf-8").find(b'echo') != -1:
+                tipo = 4
+            msg = (tipo).to_bytes(1, byteorder="big") + tamPlayer.to_bytes(1, byteorder="big") + player.encode("utf-8") + tamMsg.to_bytes(1, byteorder="big") + msg.encode("utf-8")
+            s.send(msg.encode())
+            s.sendto(msg, (ip, address))
+
+
+
+
+if __name__ == '__main__':
+    global s
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((socket.gethostname(), 3000))
+    
     try:
-        # Cria um thread para receber mensagens do chat e enviar mensagens
-        # Verifica se é o cliente 1
-        if(idx == "1"):
-            threading.Thread(target=mensagenEnvia, args=(ip, porta[0])).start()
-            threading.Thread(target=mensagemRecebe, args=(ip, porta[1])).start()
-            threading.Thread(target=mensagemRecebe, args=(ip, porta[2])).start()
-        # Verifica se é o cliente 2
-        elif(idx == "2"):
-            threading.Thread(target=mensagenEnvia, args=(ip, porta[1])).start()
-            threading.Thread(target=mensagemRecebe, args=(ip, porta[0])).start()
-            threading.Thread(target=mensagemRecebe, args=(ip, porta[2])).start()
-        # Verifica se é o cliente 3
-        elif(idx == "3"):
-            threading.Thread(target=mensagenEnvia, args=(ip, porta[2])).start()
-            threading.Thread(target=mensagemRecebe, args=(ip, porta[1])).start()
-            threading.Thread(target=mensagemRecebe, args=(ip, porta[0])).start()
+        idx = sys.argv[1]
+        
+        player = input('Qual o seu nick? ')
 
+        if len(player.encode()) > 64 or len(player.encode()) < 1:
+            print('Nome inválido')
+            sys.exit()
+
+        s.send(player.encode())
+       # while True:
+        if idx == '1':
+            threading.Thread(target=enviador, args=(player,address[0])).start()
+            threading.Thread(target=receptor, args=(player,address[1])).start()
+            threading.Thread(target=receptor, args=(player,address[2])).start()
+        if idx == '2':
+            threading.Thread(target=enviador, args=(player,address[1])).start()
+            threading.Thread(target=receptor, args=(player,address[0])).start()
+            threading.Thread(target=receptor, args=(player,address[2])).start()
+        if idx == '3':
+            threading.Thread(target=enviador, args=(player,address[2])).start()
+            threading.Thread(target=receptor, args=(player,address[0])).start()
+            threading.Thread(target=receptor, args=(player,address[1])).start()
+        
+
+
+        
     except:
-        #Retornamos uma mensagem de erro caso não seja possível criar a thread
-        print("ERRO: Erro ao criar thread!")
-
-main()
+        print('Erro ao conectar ao servidor')
---
+++ Programação com sockets UDP/UDP1/server.py
@@ -0,0 +1,28 @@
+import socket
+from threading import Thread
+
+
+def dispatch(client, player): 
+    while True:
+        try:
+            message = client.recv(1024).decode()
+            print(message)
+        except:
+            clientes.remove(client) # remove cliente da lista
+            client.close() # fecha conexão
+            break # sai do loop
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((socket.gethostname(), 3000))
+s.listen(2)
+
+clientes = list()
+
+while True:
+    client, address = s.accept()
+    clientes.append(client)
+    player = client.recv(1024).decode()
+    print(f'{player} conectou.  {address}')
+    Thread(target=dispatch, args=(client, player)).start()
+client.close()
