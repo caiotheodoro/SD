7c059dbe8303ff3f50b7ab764bb59659dd1b98c5
caiotheodoro <caio-eduardotheodoro@hotmail.com>
2022-04-19 21:18:52 -0300

    refactor: 💡 Finalized

--- Programação com sockets UDP/UDP1/cliente.py
+++ Programação com sockets UDP/UDP1/cliente.py
@@ -1,89 +1,104 @@
-import threading
 from threading import Thread
 import sys
 import socket
 
-
-
-
 ip = "127.0.0.1"
-address = [5761, 5762, 5763]
+address = [5761, 5762, 3000]
+emojis = [
+    ':-)',
+    ':-D',
+    ':-P',
+    ':-O',
+    ':-/',
+    ':-(',
+    ':-X',
+    ':-|',
+    ':->',
+    ';-)',
+    ';-D',
+    ';-P',
+    ';-O',
+    ';-/',
+    ';-(',
+    ';-X',
+    ';-|',
+] # lista de emojis
 
 
-def receptor(player, address):
-    s.bind((ip, address))
-    while True:
-        msg, addr = s.recvfrom(1024)
-        tipo = int(msg[0])
-        tamPlayer = int(msg[1])
-        player = msg[2:2+tamPlayer].decode("utf-8")
-        tamMsg = int(msg[tamPlayer+2])
-        msgPos = tamPlayer+3
-        msg = msg[msgPos:msgPos+tamMsg].decode("utf-8")
+def receptor(player, adr):
+    s.bind((ip, int(adr))) # binda o socket ao ip e a porta
+    while True: 
+        msg, addr = s.recvfrom(1024) # recebe uma mensagem
+        tipo = int(msg[0]) # pega o tipo da mensagem
+        tamPlayer = int(msg[1]) # pega o tamanho do player
+        player = msg[2:2+tamPlayer].decode("utf-8") # pega o player
+        tamMsg = int(msg[tamPlayer+2]) # pega o tamanho da mensagem
+        msg = msg[tamPlayer+3:tamPlayer+3+tamMsg].decode("utf-8") # pega a mensagem
 
-        if tipo == 4:
-            msg = (1).to_bytes(1, byteorder="big") + tamPlayer.to_bytes(1, byteorder="big") + player.encode("utf-8") + tamMsg.to_bytes(1, byteorder="big") + msg.encode("utf-8")
-            s.sendto(msg, (ip, address))
-        else:
-            print(f"{player}: {msg}")
-        
+        if tipo == 4: # se for um echo
+            msg = (1).to_bytes(1, byteorder="big") + tamPlayer.to_bytes(1, byteorder="big") + player.encode("utf-8") + tamMsg.to_bytes(1, byteorder="big") + msg.encode("utf-8") # cria a mensagem
+            print(f"{player} realizou echo") # imprime o echo
+            s.sendto(msg, addr) # envia a mensagem para o cliente
+        elif tipo == 1: # se for uma mensagem
+            print(f"{player}: {msg}") # imprime a mensagem
+        elif tipo == 2: # se for um emoji
+            print(f"{player}  emoji: {msg}") # imprime o emoji
+        elif tipo == 3: # se for uma url
+            print(f"{player}  link: {msg}") # imprime o link
 
 
 
-def enviador(player, address):
-    tipo = 1
-    while True:
-        msg = input("~ ")
-        tamMsg = len(msg.encode("utf-8"))
-        tamPlayer = len(player.encode("utf-8"))
-        if len(msg.encode("utf-8")) > 254:
-            print("Mensagem muito grande")
-            continue
-        else:  
-            if msg.encode("utf-8").find(b'\xF0\x9F\x98\x81') != -1:
+def enviador(player, adr):
+    while True: 
+        msg = input("~ ") # recebe uma mensagem
+        tipo = 1 # tipo da mensagem
+        tamMsg = len(msg.encode("utf-8")) # tamanho da mensagem
+        tamPlayer = len(player.encode("utf-8")) # tamanho do player
+        if len(msg.encode("utf-8")) > 254: # se a mensagem for maior que 254
+            print("Mensagem muito grande") # imprime mensagem
+            continue # volta para o inicio do loop
+        else:        # se não
+            if msg in emojis: # se a mensagem for um emoji
                 tipo = 2
-            if msg.encode("utf-8").find(b'http') != -1:
+            if msg.encode("utf-8").find(b'http')  != -1: # se a mensagem for uma url
                 tipo = 3
-            if msg.encode("utf-8").find(b'echo') != -1:
+            if msg.encode("utf-8").find(b'echo') != -1: # se a mensagem for um echo
                 tipo = 4
-            msg = (tipo).to_bytes(1, byteorder="big") + tamPlayer.to_bytes(1, byteorder="big") + player.encode("utf-8") + tamMsg.to_bytes(1, byteorder="big") + msg.encode("utf-8")
-            s.send(msg.encode())
-            s.sendto(msg, (ip, address))
+            msg = (tipo).to_bytes(1, byteorder="big") + tamPlayer.to_bytes(1, byteorder="big") + player.encode("utf-8") + tamMsg.to_bytes(1, byteorder="big") + msg.encode("utf-8") # cria a mensagem
+          #  s.send(msg.encode())
+            s.sendto(msg, (ip, adr))    # envia a mensagem para o cliente
 
-
-
+# def server():
+#     global sock
+#     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#     sock.bind((ip, address[2]))
+#     while True:
+#         msg, addr = sock.recvfrom(1024)
 
 if __name__ == '__main__':
     global s
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((socket.gethostname(), 3000))
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  #  s.connect((socket.gethostname(), 3000))
     
     try:
         idx = sys.argv[1]
         
         player = input('Qual o seu nick? ')
 
-        if len(player.encode()) > 64 or len(player.encode()) < 1:
+        if len(player.encode()) > 64 or len(player.encode()) < 1: # se o player for maior que 64 ou menor que 1
             print('Nome inválido')
             sys.exit()
 
-        s.send(player.encode())
+    #    s.send(player.encode())
        # while True:
-        if idx == '1':
-            threading.Thread(target=enviador, args=(player,address[0])).start()
-            threading.Thread(target=receptor, args=(player,address[1])).start()
-            threading.Thread(target=receptor, args=(player,address[2])).start()
+        if idx == '1':  
+            Thread(target=enviador, args=(player,address[0])).start() # inicia o enviador
+            Thread(target=receptor, args=(player,address[1])).start() # inicia o receptor
+            # Thread(target=server).start()
         if idx == '2':
-            threading.Thread(target=enviador, args=(player,address[1])).start()
-            threading.Thread(target=receptor, args=(player,address[0])).start()
-            threading.Thread(target=receptor, args=(player,address[2])).start()
-        if idx == '3':
-            threading.Thread(target=enviador, args=(player,address[2])).start()
-            threading.Thread(target=receptor, args=(player,address[0])).start()
-            threading.Thread(target=receptor, args=(player,address[1])).start()
-        
-
-
-        
+            Thread(target=enviador, args=(player,address[1])).start() # inicia o enviador
+            Thread(target=receptor, args=(player,address[0])).start() # inicia o receptor
+            # Thread(target=server).start()
+   
     except:
         print('Erro ao conectar ao servidor')
--- Programação com sockets UDP/UDP1/server.py
+++ Programação com sockets UDP/UDP1/server.py
@@ -1,28 +1,28 @@
-import socket
-from threading import Thread
+# import socket
+# from threading import Thread
 
 
-def dispatch(client, player): 
-    while True:
-        try:
-            message = client.recv(1024).decode()
-            print(message)
-        except:
-            clientes.remove(client) # remove cliente da lista
-            client.close() # fecha conexão
-            break # sai do loop
+# def dispatch(client, player): 
+#     while True:
+#         try:
+#             message = client.recv(1024).decode()
+#             print(message)
+#         except:
+#             clientes.remove(client) # remove cliente da lista
+#             client.close() # fecha conexão
+#             break # sai do loop
 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 3000))
-s.listen(2)
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s.bind((socket.gethostname(), 3000))
+# s.listen(2)
 
-clientes = list()
+# clientes = list()
 
-while True:
-    client, address = s.accept()
-    clientes.append(client)
-    player = client.recv(1024).decode()
-    print(f'{player} conectou.  {address}')
-    Thread(target=dispatch, args=(client, player)).start()
-client.close()
+# while True:
+#     client, address = s.accept()
+#     clientes.append(client)
+#     player = client.recv(1024).decode()
+#     print(f'{player} conectou.  {address}')
+#     Thread(target=dispatch, args=(client, player)).start()
+# client.close()
